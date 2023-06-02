32d55241e3573d69cfe2c142699f201536203317
Keshav-Aneja <keshav.aneja09@gmail.com>
2023-06-02 18:50:16 +0530

    Main Index Page completed

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log("Intersecting");
+      entry.target.classList.add("show");
+      entry.target.classList.remove("head-left-pro");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const project = document.querySelectorAll(".head-left-pro");
+project.forEach((p) => observer.observe(p));
+const imge = document.querySelectorAll(".head-left-pro-2");
+imge.forEach((p) => observer.observe(p));
+
+const cards = document.querySelectorAll(".card");
+const desc = document.querySelectorAll(".desc");
+
+for (let i = 0; i < 4; i++) {
+  cards[i].addEventListener("mouseover", function () {
+    desc[i].classList.remove("desc");
+  });
+  cards[i].addEventListener("mouseout", function () {
+    desc[i].classList.add("desc");
+  });
+}
